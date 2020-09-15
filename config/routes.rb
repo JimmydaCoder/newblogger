1612ebf54875eaf2f8500378c0e02e9a42de760a
minh <minh220500@gmail.com>
2020-09-15 14:43:26 +0700

    form done

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+<<<<<<< HEAD
+  root 'cars#index'
+  resources :cars
+
+=======
+  # For details on bithe DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+>>>>>>> cd977b5... first blogger commit
+end
